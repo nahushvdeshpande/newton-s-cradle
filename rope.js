44321547897488945a07891c346ab1960bfc2d39
nahushvdeshpande <73283814+nahushvdeshpande@users.noreply.github.com>
2020-11-22 23:55:38 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,15 @@
+class rope{
+constructor(body1,body2,offsetX,offsetY)
+{
+	this.offsetX=offsetX
+	this.offsetY=offsetY
+	var options={
+		bodyA:body1,
+		bodyB:body2,
+		pointB:{x:this.offsetX,y:this.offsetY}
+	}
+	//console.log(options)
+	this.rope=Constraint.create(options)
+	World.add(world,this.rope)
+}
+}
